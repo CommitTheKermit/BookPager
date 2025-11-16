24095890842ea71e1f7e2d0ae0611eb45fe553dd
ujeong <qnf323@naver.com>
2025-11-16 14:30:40 +0900

    design(ManualPager): 책 수동 넘기기 버튼 구현

---
+++ app/src/main/java/com/friendshiphyun/bookpager/ui/ManualPager.kt
@@ -0,0 +1,88 @@
+package com.friendshiphyun.bookpager.ui
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowForward
+import androidx.compose.material.icons.automirrored.filled.MenuBook
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.friendshiphyun.bookpager.ui.theme.Colors
+
+@Preview
+@Composable
+fun ManualPager() {
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = 10.dp, vertical = 20.dp)
+            .background(
+                color = Colors.primarySub, shape = RoundedCornerShape(12.dp)
+            )
+    )
+    {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(vertical = 10.dp)
+        ) {
+            Icon(
+                imageVector = Icons.AutoMirrored.Default.MenuBook,
+                tint = Colors.primaryMain,
+                contentDescription = null,
+                modifier = Modifier.size(64.dp)
+
+            )
+            Button(
+                onClick = {},
+                enabled = true,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 30.dp, vertical = 10.dp)
+                    .height(45.dp),
+
+
+                shape = RoundedCornerShape(12.dp),
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = Colors.primaryMain,
+                    contentColor = Color.White
+                )
+            ) {
+                if (false) {
+                    CircularProgressIndicator(
+                        modifier = Modifier.size(24.dp),
+                        color = MaterialTheme.colorScheme.onPrimary
+                    )
+                } else {
+                    Icon(
+                        Icons.AutoMirrored.Filled.ArrowForward,
+                        contentDescription = null
+                    )
+                    Spacer(modifier = Modifier.width(8.dp))
+                    Text("넘기기", style = MaterialTheme.typography.titleMedium)
+                }
+            }
+
+        }
+
+    }
+}
