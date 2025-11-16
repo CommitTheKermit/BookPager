4d497bb6cbca691be244869855cc1fc2b95644e6
ujeong <qnf323@naver.com>
2025-11-16 14:07:41 +0900

    chore(color): 컬러 최신화

--- app/src/main/java/com/friendshiphyun/bookpager/ui/theme/Color.kt
+++ app/src/main/java/com/friendshiphyun/bookpager/ui/theme/Color.kt
@@ -2,10 +2,12 @@ package com.friendshiphyun.bookpager.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+val primaryMain = Color(0xFF1E4A75)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val primarySub = Color(0xFFB4BCDA)
+
+val primaryTextColor = Color(0xFF363639)
+
+val highlightedTextColor = Color(0xFF363639)
+
+val commonWhite = Color(0xffFFFFFF)
