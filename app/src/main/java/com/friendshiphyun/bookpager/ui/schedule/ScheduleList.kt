58ea2c66708df0eaf1359ebd9c2dec83034ef0fc
CommitTheKermit <qnf323@naver.com>
2025-11-17 00:12:00 +0900

    design(schedule): 여백 추가

--- app/src/main/java/com/friendshiphyun/bookpager/ui/schedule/ScheduleList.kt
+++ app/src/main/java/com/friendshiphyun/bookpager/ui/schedule/ScheduleList.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Button
@@ -25,9 +26,9 @@ import java.time.LocalTime
 fun ScheduleList(
     schedules: List<Schedule> = emptyList(),
 ) {
-
-
-    Column() {
+    Column(
+        modifier = Modifier.padding(vertical = 15.dp, horizontal = 20.dp)
+    ) {
         Row(
             verticalAlignment = Alignment.CenterVertically,
             horizontalArrangement = Arrangement.SpaceBetween,
